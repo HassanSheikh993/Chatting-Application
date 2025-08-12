691fe5ba5d453124a77c4831791a7abdd5d2ee45
Hassan <hassansheikh993@gmail.com>
2025-08-12 13:20:49 +0500

    update generateToken file

--- back-end/config/generateToken.js
+++ back-end/config/generateToken.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.JWT_SECRET)
+
 export const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
